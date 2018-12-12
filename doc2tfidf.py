570fb001752c30e0945cff8e26b84fb71411d7ef
NanzhenK <nanzhen_kuang@hotmail.com>
2018-12-12 09:21:28 +0800

    Create doc2tfidf.py

---
+++ doc2tfidf.py
@@ -0,0 +1,19 @@
+    
+    import numpy as np
+    
+    def doc2onehot_tfidf(self, data):
+        onehot = np.zeros((len(data), len(self.__voc)))
+        tf = np.zeros((len(data), len(self.__voc)))
+        for d, sentence in enumerate(data):
+            for word in sentence:
+                if word in self.__voc:
+                    pos = self.__voc.index(word)
+                    onehot[d][pos] = 1
+                    tf[d][pos] += 1
+        row_sum = tf.sum(axis=1)+1
+        tf = tf / row_sum[:, np.newaxis]
+        ndw = onehot.sum(axis=0)
+        idf = list(map(lambda x: np.log10(self.len_data) / (x+1), ndw))
+        tfidf = tf * np.array(idf)
+        res = np.asarray(tfidf, dtype='f')
+        return res
